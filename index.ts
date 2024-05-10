c86c223cf0b662dcaa148d676e8bb21024defcd0
Charles "Blue" Hartsell <98487762+tealblu@users.noreply.github.com>
2024-05-10 16:49:56 -0400

    switched to dynamic input parsing

--- index.ts
+++ index.ts
@@ -2,16 +2,54 @@ import "@logseq/libs"
 
 // main function
 function main () {
-    logseq.App.showMsg('Dice Roller loaded :)')
-
-    // Add slash command
+    // add slash command for dynamic command parsing (e.g. /roll 2d6+10)
     logseq.Editor.registerSlashCommand(
-        'roll d100',
+        'roll',
         async () => {
-            const roll = Math.floor(Math.random() * 100) + 1
-            await logseq.Editor.insertAtEditingCursor(`d100: ${roll}`)
+            // get content of current block
+            const content = await logseq.Editor.getEditingBlockContent()
+
+            // parse content for dice roll
+            const roll = content.match(/(\d*)d(\d+)([+-]\d+)?/)
+
+            // if no dice roll found, return
+            if (!roll) {
+                logseq.App.showMsg('No dice roll found :(')
+                return
+            }
+
+            // extract dice roll parameters
+            const [, num, dice, modifier] = roll
+
+            // convert num to integer
+            const numInt = num ? parseInt(num) : 1
+
+            // roll dice and track all results
+            const rolls = []
+            for (let i = 0; i < numInt; i++) {
+                rolls.push(Math.floor(Math.random() * parseInt(dice)) + 1)
+            }
+
+            // calculate total
+            let total = rolls.reduce((a, b) => a + b, 0)
+
+            // apply modifier
+            if (modifier) {
+                total += parseInt(modifier)
+            }
+
+            // insert result into block
+            let result = `: ${total}`
+            if (numInt > 1) {
+                result += ` (${rolls.join(', ')})`
+            }
+
+            // insert result into block
+            logseq.Editor.insertAtEditingCursor(result)
         }
     )
+
+    logseq.App.showMsg('Dice Roller loaded :)')
 }
 
 logseq.ready(main).catch(console.error)
